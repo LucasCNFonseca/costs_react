ef8bf44042210986f72c261b317238951bf30d6f
t-lucas.nunes <t-lucas.nunes@cni.com.br>
2024-08-26 06:19:46 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+function App() {
+  return (
+    <Router>
+      
+      <Navbar />
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
