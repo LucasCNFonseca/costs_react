5417e6baa32de6f9725de1495575f862b72aba70
LucasCNFonseca <lucas.cnf2@outlook.com>
2024-08-28 13:07:57 -0300

    Create NewProject page

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,14 @@
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
+
 function NewProject () {
-    return <h1>Novo Projeto</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm />
+        </div>
+    )
 }
 
 export default NewProject
