1c8b35354876bb3ef01aedab840d2847ab127209
LucasCNFonseca <lucas.cnf2@outlook.com>
2024-08-30 11:04:24 -0300

    Update NewProject Page

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,14 +1,39 @@
-import styles from './NewProject.module.css'
-import ProjectForm from '../project/ProjectForm'
+import { useNavigate } from "react-router-dom";
 
-function NewProject () {
-    return (
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto" />
-        </div>
-    )
+import styles from "./NewProject.module.css";
+import ProjectForm from "../project/ProjectForm";
+
+function NewProject() {
+  const navigate = useNavigate();
+
+  function creatPost(project) {
+    // initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        // redirect
+        navigate("/projects", { message: "Projeto criado com sucesso!" })
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={creatPost} btnText="Criar Projeto" />
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
